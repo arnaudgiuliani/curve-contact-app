51a20bcad1281505f2f1912a9409cb837f487602
Arnaud Giuliani <agiuliani@ekito.fr>
2018-09-05 16:27:47 +0200

    fix start as normal

--- app/src/main/java/com/imaginecurve/curvecontactsapp/MainApplication.kt
+++ app/src/main/java/com/imaginecurve/curvecontactsapp/MainApplication.kt
@@ -3,7 +3,6 @@ package com.imaginecurve.curvecontactsapp
 import android.app.Application
 import com.imaginecurve.curvecontactsapp.di.appModule
 import com.imaginecurve.curvecontactsapp.di.dataModule
-import com.imaginecurve.curvecontactsapp.di.mockedDataModule
 import org.koin.android.ext.android.startKoin
 
 
@@ -16,7 +15,7 @@ class MainApplication : Application() {
 //        startKoin(this, listOf(appModule, mockedDataModule))
 
         // Star Koin for DI
-        startKoin(this, listOf(appModule, mockedDataModule))
+        startKoin(this, listOf(appModule, dataModule))
 
     }
 }
